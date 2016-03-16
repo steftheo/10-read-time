b6c58f610abcba130f61be3a2c1222e6cb1dd201
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-16 12:42:14 -0500

    Create tests for num words

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 'use strict';
+/* globals numWords, readTime */
---
+++ tests/num-words-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* globals numWords */
+
+QUnit.module(`numWords`);
+
+test(`numWords is a function`, (assert) => {
+  assert.ok(numWords, `'numWords' should exist`);
+  assert.equal(typeof numWords, `function`,
+    `'numWords' should be a named function (not assigned to a variable)`);
+
+  assert.equal(numWords.length, 1,
+    `'numWords' should accept an argument: a string of words from an article`);
+});
+
+test(`numWords can calculate number of words in a string`, (assert) => {
+  const word = `Foo`;
+  const words = `Lorem ipsum dolor sit amet, consectetur adipisicing elit.` +
+    ` Eaque reiciendis veritatis laborum, laboriosam ab explicabo delectus!` +
+    ` Repellendus, natus. Eveniet perspiciatis temporibus consectetur nemo` +
+    `fugiat molestias deleniti neque ad consequuntur quod?`;
+
+  assert.equal(numWords(word), 1,
+    `'numWords' can know that there is only one word in "${word}"`);
+  assert.equal(numWords(words), 30,
+    `'numWords' can calculate the number of words in longer strings "${words}"`);
+});
